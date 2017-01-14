72d9d1f3736cd411d43526bbd243b84d4016cc37
Brad <brad@Bradui-MacBook-Pro.local>
2017-01-14 17:11:13 +0900

    add templates

--- django_prj/src/blog/views.py
+++ django_prj/src/blog/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
+# from random import randint
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
-
+    # random_number = randint(1,10)
+    # return HttpResponse("Hello, world. {}".format(random_number))
+    # return HttpResponse("Hello, world. You're at the index.")
+    name = "Brad"
+    return render(request, "index.html", { "name" : name })
